095d329597849d0fc665c803eec17f0ef15578fa
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-17 14:40:20 -0600

    Created: Products, Brands, Categories : Models,Seeders, Factories and Controllers

---
+++ api/database/migrations/2023_07_17_194053_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->string('image');            
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->decimal('price_wholesale', $precision = 8, $scale = 2);
+            $table->decimal('price_distributor', $precision = 8, $scale = 2);
+            $table->decimal('price_box', $precision = 8, $scale = 2);
+            $table->decimal('price_vip', $precision = 8, $scale = 2);
+            $table->integer('pieces_by_box');
+            $table->integer('stock');
+            $table->integer('min_stock');
+            $table->integer('disabled');
+
+            $table->unsignedBigInteger('brand_id');            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
