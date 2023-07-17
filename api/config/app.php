21f77e5be77c12fd20322c3d7048a0c00479e7ec
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-17 13:32:48 -0600

    Jwt working

--- api/config/app.php
+++ api/config/app.php
@@ -168,6 +168,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * JSON WEB TOKEN
+        */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ])->toArray(),
 
     /*
@@ -183,6 +189,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        // * Customs
+        //'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        //'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ])->toArray(),
 
 ];
