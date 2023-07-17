288f824b95ee2370dc80bc89d22446fdc4c80e4d
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-17 16:31:24 -0600

    Edited: migration of categories

--- api/database/migrations/2023_07_17_200608_create_categories_table.php
+++ api/database/migrations/2023_07_17_200608_create_categories_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('category_id');    
+            $table->unsignedBigInteger('parent_id')->nullable();    
             $table->integer('disabled');
             $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories');
         });
     }
 
