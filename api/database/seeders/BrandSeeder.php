0edcda1d4e304101c850203afc88425842374c6d
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-17 16:30:47 -0600

    Created: Seeder of brands

--- api/database/seeders/BrandSeeder.php
+++ api/database/seeders/BrandSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Brand;
 
 class BrandSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Brand::create(['name'=>'LinkBits','disabled'=>1]);
+        Brand::create(['name'=>'Mega Luz','disabled'=>1]);
+        Brand::create(['name'=>'HP','disabled'=>1]);
+        Brand::create(['name'=>'Kingston','disabled'=>1]);
+        Brand::create(['name'=>'ASUS','disabled'=>1]);
+        Brand::create(['name'=>'Apple','disabled'=>1]);
     }
 }
