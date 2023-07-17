83a14b98238841750fb4c53a431cdbd1bd35851f
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-17 16:31:37 -0600

    Seeder of categories

--- api/database/seeders/CategorySeeder.php
+++ api/database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create(["name"=>"Bocinas","parent_id"=>null,"disabled"=>1]);
+        Category::create(["name"=>"Almacenamiento","parent_id"=>null,"disabled"=>1]);
+        Category::create(["name"=>"Hogar","parent_id"=>null,"disabled"=>1]);
+        Category::create(["name"=>"Iluminación","parent_id"=>null,"disabled"=>1]);
+        Category::create(["name"=>"Cámaras de Vigilancia","parent_id"=>null,"disabled"=>1]);
     }
 }
