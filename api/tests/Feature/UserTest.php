c2c2f7a4ae8ad775ca2b9e9902df730027753918
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-18 12:28:37 -0600

    Test for login routes

--- api/tests/Feature/UserTest.php
+++ api/tests/Feature/UserTest.php
@@ -5,20 +5,98 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
 
 class UserTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
+{   
+    /*
+     * TEST FOR SUCCESS LOGIN OF THE USER 
+     * 
+     *  @return void
+    */
     public function test_user_route_login(): void
-    {
-        //$response = $this->post('/api/v1/auth/login');
-        
-        $response = $this->withHeaders([
-            'X-Header' => 'Value',
-        ])->post('/api/v1/auth/login', ['email' => 'kaelyn.berge@example.org','password'=>'password']);
+    {      
+        $baseUrl = '/api/v1/auth/login';     
+        $email = "ward.ciara@example.net";
+        $password = "password";
+       
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $password
+        ]);
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token', 'token_type', 'expires_in'
+            ]);
+    }
+
+    /*
+     * TEST FOR SUCCESS LOGIN OF THE USER 
+     * 
+     * @return void
+     */
+    public function test_wrong_password_user_route_login(): void
+    {      
+        $baseUrl = '/api/v1/auth/login';     
+        $email = "ward.ciara@example.net";
+        $password = "abcd";
+       
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $response
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                'error'
+            ]);
+    }
+    /*
+     * TEST FOR SUCCESS LOGIN OF THE USER 
+     * 
+     * @return void
+     */
+    public function test_wrong_email_user_route_login(): void
+    {      
+        $baseUrl = '/api/v1/auth/login';     
+        $email = "wrong-mail.ciara@example.net";
+        $password = "password";
+       
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $response
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                'error'
+            ]);
+    }
+    /*
+     * TEST FOR SUCCESS LOGIN OF THE USER 
+     * 
+     * @return void
+     */
+    public function test_empty_values_user_route_login(): void
+    {      
+        $baseUrl = '/api/v1/auth/login';     
+        $email = "";
+        $password = "";
+       
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $response
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                'error'
+            ]);
     }
 }
