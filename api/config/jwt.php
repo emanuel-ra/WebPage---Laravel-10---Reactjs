3f3e3bce624bee62fff61d45b87b560fc16910a6
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-21 11:25:17 -0600

    jwt config

--- api/config/jwt.php
+++ api/config/jwt.php
@@ -272,8 +272,7 @@ return [
         |
         */
 
-        //'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
-        'jwt' => Tymon\JWTAuth\Providers\JWT\Namshi::class ,
+        'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
 
         /*
         |--------------------------------------------------------------------------
